9d887009c7a75a09424a905035da0ae728f16c53
Wu Hsiu Rong <mandy19970219@gm.student.ncut.edu.tw>
2017-10-12 19:15:43 +0800

    建立3個rounte

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,23 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function (){
+    //return view('home');
+//});
+    //Route::get('/about',function(){
+       //return view('about');
+    //});
+        //Route::get('/news',function(){
+            //return view('news');
+        //});
+
+Route::get('home',['as'=>'home.index',function(){
+    return view('home');
+}]);
+
+Route::get('about',['as'=>'about.index',function(){
+    return view('about');
+}]);
+Route::get('news',['as'=>'news.index',function(){
+    return view('news');
+}]);
